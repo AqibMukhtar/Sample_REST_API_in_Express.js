113b11f5c14fafe36f9a14dc19640a17b794049f
AqibMukhtar <aqibmukhtar08@gmail.com>
2019-08-26 15:56:18 +0500

    Post request handlers completed for creation of data
    Added body-parser middleware, concept of meta data in resources, 2 post handlers to create resource.  If category of particular resource doesnot exist than that category must be created to added resource of that category. Added more functions in logic.js file along with some alterations of existing ones.

--- logic.js
+++ logic.js
@@ -2,7 +2,7 @@
 
 function checkCategory(category, categories) {
     let x = categories.length;
-    while (x-- > 0) {
+    while (x --> 0) {
         if (categories[x].name === category)
             return {
                 status: true,
@@ -22,21 +22,68 @@ function checkId(id, category, categories) {
         while (i --> 0) {
             if (content[i].id === id) {
                 return {
-                    status          : true,
-                    categoryIndex   : x.index,
-                    contentIndex    : i
+                    status: true,
+                    categoryIndex: x.index,
+                    contentIndex: i,
+                    memberStatus: true
                 };
             }
         }
         return {
-            status: false
+            status: false,
+            categoryStatus: true,
+            memberStatus: false
         };
     }
     else
         return {
-            status: false
+            status: false,
+            categoryStatus: false
         };
 }
 
-module.exports = { checkCategory,
-checkId };
+function appendResource(category, categories, keyValues) {
+    let contentArray = [{}];
+    contentArray[0].id = undefined;
+    for (let x in keyValues) contentArray[0][(keyValues[x])] = undefined;
+    let newCategory = {
+        name: category,
+        content: contentArray
+    };
+    categories.push(newCategory);
+}
+
+function appendMember(categories, category, keyValues, id) {
+    const categoryIndex = checkCategory(category, categories).index;
+    const incommingKeys = Object.keys(keyValues);
+    let existingKeys = Object.keys(categories[categoryIndex].content[0]);
+    existingKeys.shift();
+    if (JSON.stringify(existingKeys) === JSON.stringify(incommingKeys)) {
+        let newMember = {};
+        newMember.id = id;
+        for (let key in keyValues) newMember[key] = keyValues[key];
+        categories[categoryIndex].content.push(newMember);
+        return {
+            creationStatus : true,
+            createdResource: newMember
+        }
+    }
+    else return {
+        creationStatus : false,
+        createdResource: undefined
+    }
+}
+
+function parseResource(originalResource) {
+    let arr = JSON.parse(JSON.stringify(originalResource));
+    arr.shift();
+    return JSON.stringify(arr);
+}
+
+module.exports = {
+    checkCategory,
+    checkId,
+    appendResource,
+    appendMember,
+    parseResource
+};
--- rest.js
+++ rest.js
@@ -1,18 +1,21 @@
 "use strict";
 const Logic = require('./logic');
+const BodyParser = require('body-parser');
 const Express = require('express');
 const app = Express();
 
 // Existing resources
-
+    // categories[anyIndex].content[0] defines meta data
 let students = [
-    { id: 'S1', name: 'Aqib Mukhtar', projectNo: 32 },
-    { id: 'S2', name: 'Abdul Rehman', projectNO: 49 },
-    { id: 'S3', name: 'Muhammad Uzair', projectNO: 53 }
+    { id: undefined, name: undefined, projectNO: undefined},     
+    { id: 'S1', name: 'Aqib Mukhtar', projectNO: '32' },
+    { id: 'S2', name: 'Abdul Rehman', projectNO: '49' },
+    { id: 'S3', name: 'Muhammad Uzair', projectNO: '53' }
 ],
     instructors = [
-        { id: 'I1', name: 'Abdullah Sohail', completedProjects: 7 },
-        { id: 'I2', name: 'Anus Baig', completedProjects: 10 }
+        { id: undefined, name: undefined, completedProjects: undefined },
+        { id: 'I1', name: 'Abdullah Sohail', completedProjects: '7' },
+        { id: 'I2', name: 'Anus Baig', completedProjects: '10' }
     ];
 
 let categories = [
@@ -24,31 +27,67 @@ let categories = [
 
 app.use((req, res, next) => {
     res.set({
-        'Content-Type' : 'text/json'
+        'Content-Type': 'text/json'
     });
     next();
 });
+app.use(BodyParser.urlencoded({ 'extended': 'false' }));
 
 // GET requests handlers to retriving resources
 
 app.get('/api/:category', (req, res) => {
     const authentic = Logic.checkCategory(req.params.category, categories);
-    if (authentic.status) {
-        res.status(200).send(JSON.stringify(categories[authentic.index].content));
-    }
-    else {
-        res.status(404).send(JSON.stringify({message : 'Resource not found'}));
-    }
+    if (authentic.status)
+        res.status(200).send(Logic.parseResource(categories[authentic.index].content));
+    else 
+        res.status(404).send(JSON.stringify({ message: 'Resource not found' }));
 });
 
 app.get('/api/:category/:id', (req, res) => {
     const authentic = Logic.checkId(req.params.id, req.params.category, categories);
-    if (authentic.status) {
+    if (authentic.status)
         res.status(200).send(JSON.stringify(categories[authentic.categoryIndex].content[authentic.contentIndex]));
+    else
+        res.status(404).send(JSON.stringify({ message: 'Resource not found' }));
+});
+
+// POST request handlers to create resdources
+
+app.post('/api/:category', (req, res) => {
+    const keyValues = req.body;
+    const category = req.params.category;
+    const authentic = Logic.checkCategory(category, categories);
+    res.set({
+        'Cache-Control': 'no-cache'
+    });
+    if (!authentic.status) {
+        Logic.appendResource(category, categories, keyValues);
+        res.status(201).send(JSON.stringify(categories[categories.length - 1].content));
     }
-    else {
-        res.status(404).send(JSON.stringify({message : 'Resource not found'}));
+    else
+        res.status(200).send(JSON.stringify({ message: 'Resource already exist' }));
+});
+
+app.post('/api/:category/:id', (req, res) => {
+    const category = req.params.category, id = req.params.id, attributes = req.body;
+    const authentic = Logic.checkId(id, category, categories);
+    res.set({
+        'Cache-Control': 'no-cache'
+    });
+    if (!authentic.status) {
+        if (!authentic.categoryStatus) {
+            res.status(200).send(JSON.stringify({ message: `${category} does not exist` }));
+        }
+        else if (!authentic.memberStatus) {
+            const newResource = Logic.appendMember(categories, category, attributes, id)
+            if (newResource.creationStatus)
+                res.status(201).send(JSON.stringify(newResource.createdResource));
+            else
+                res.status(200).send(JSON.stringify({ message: 'Request badly constructed' }));
+        }
     }
+    else
+        res.status(200).send(JSON.stringify({ message: 'Resource already exist' }));
 });
 
 // Port settings
